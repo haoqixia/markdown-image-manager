80e4a461f91207fca20843b3dce5346ca979e5b1
haoqixia <xjn-yz@163.com>
2020-04-08 18:02:46 +0800

    s

--- spec/markdown-image-manager-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MarkdownImageManagerView from '../lib/markdown-image-manager-view';
-
-describe('MarkdownImageManagerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});

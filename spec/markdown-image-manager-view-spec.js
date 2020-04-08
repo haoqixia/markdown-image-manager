5a65d21d529720d191cc001726481c86cf7d5644
haoqixia <xjn-yz@163.com>
2020-04-08 17:41:25 +0800

    first commit

---
+++ spec/markdown-image-manager-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MarkdownImageManagerView from '../lib/markdown-image-manager-view';
+
+describe('MarkdownImageManagerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
